01aae5b2b95324767d8d89e68322e5da5d899525
Andrey Vorotnikov <AndrewVorotnikov@yandex.ru>
2022-09-19 22:44:11 +0300

    Add trend lines

--- time_series/generators.py
+++ time_series/generators.py
@@ -4,5 +4,14 @@ import numpy as np
 def sample1(n : int, h = 0.05):
     return np.array([np.sqrt(k * h) + np.random.normal() for k in range(n)])
 
+
+def sample1_trend(n : int, h = 0.05):
+    return np.array([np.sqrt(k * h) for k in range(n)])
+
+
 def sample2(n : int, h = 0.1):
     return np.array([0.5 * np.sin(k * h) + np.random.normal() for k in range(n)])
+
+
+def sample2_trend(n : int, h = 0.1):
+    return np.array([0.5 * np.sin(k * h) for k in range(n)])
